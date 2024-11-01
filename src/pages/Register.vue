4aee94396cb90e1909af965d9fee03f55279c376
A252dev <a252p@outlook.com>
2024-11-01 22:22:44 +0100

    added the registration

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,7 +1,35 @@
 <script setup>
 
 import '@/assets/styles/reg_log.css'
+import router from '@/router';
+import authService from '@/services/auth.service';
 
+</script>
+
+<script>
+
+export default {
+    data() {
+        return {
+            registerData: {
+                email: '',
+                firstName: '',
+                secondName: '',
+                birthday: '',
+                password: ''
+            },
+            repeatPassword: ''
+        }
+    },
+    methods: {
+        onSubmit(registerData) {
+            if (this.registerData.password != this.repeatPassword) console.log('password error');
+            authService.register(registerData);
+            this.registerData = '';
+            router.push('/login');
+        }
+    }
+}
 </script>
 
 <template>
@@ -9,28 +37,34 @@ import '@/assets/styles/reg_log.css'
         <form class="form">
             <div class="data__container">
                 <h5 class="form__title">First name</h5>
-                <input class="form__input" name="firstName" type="text" />
+                <input class="form__input" v-bind:value="registerData.firstName"
+                    @input="registerData.firstName = $event.target.value" type="text" />
             </div>
             <div class="data__container">
                 <h5 class="form__title">Second name</h5>
-                <input class="form__input" name="secondName" type="text" />
+                <input class="form__input" v-bind:value="registerData.secondName"
+                    @input="registerData.secondName = $event.target.value" type="text" />
             </div>
             <div class="data__container">
                 <h5 class="form__title">Birthday</h5>
-                <input class="form__input" name="birthday" type="date" />
+                <input class="form__input" v-bind:value="registerData.birthday"
+                    @input="registerData.birthday = $event.target.value" type="date" />
             </div>
             <div class="data__container">
                 <h5 class="form__title">Email</h5>
-                <input class="form__input" name="email" type="email" />
+                <input class="form__input" v-bind:value="registerData.email"
+                    @input="registerData.email = $event.target.value" type="email" />
             </div>
             <div class="data__container">
                 <h5 class="form__title">Password</h5>
-                <input class="form__input" name="password" type="password" />
+                <input class="form__input" v-bind:value="registerData.password"
+                    @input="registerData.password = $event.target.value" type="password" />
                 <p class="form__subtitle">The minimum password length is 8 characters.</p>
             </div>
             <div class="data__container">
                 <h5 class="form__title">Confirm password</h5>
-                <input class="form__input" name="repeatPassword" type="password" />
+                <input class="form__input" v-bind:value="repeatPassword" @input="repeatPassword = $event.target.value"
+                    type="password" />
             </div>
             <div class="data__container">
                 <h5 class="form__title">I am 13 years of age or older and agree to the terms of the <a
@@ -42,7 +76,7 @@ import '@/assets/styles/reg_log.css'
                 <h5 class="form__title">Keep login</h5>
                 <input class="checkbox" name="keepLogin" type="checkbox" />
             </div>
-            <input class="button" type="button" value="Register" />
+            <input class="button" type="button" @click="onSubmit(registerData)" value="Register" />
             <RouterLink class="register__button" to="/login">Have account? Login</RouterLink>
         </form>
     </section>
